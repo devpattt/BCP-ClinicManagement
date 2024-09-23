261cb6b338a086c9dda42d85ddb8c8341fa1ef4f
Pat <patsandesu@gmail.com>
2024-09-24 01:55:42 +0800

    modify

--- pages-blank.php
+++ pages-blank.php
@@ -133,7 +133,7 @@
     <hr class="sidebar-divider">
 
       <li class="nav-item">
-        <a class="nav-link " href="index.html">
+        <a class="nav-link " href="blankindex.php">
           <i class="bi bi-grid"></i>
           <span>Dashboard</span>
         </a>
@@ -169,7 +169,7 @@
             </a>
           </li>
           <li>
-              <a href="pages-blank.php">
+              <a href="blankanomaly.php">
                 <i class="bi bi-circle" ></i><span>A.I Anomaly</span>
               </a>
             </li>
@@ -177,9 +177,9 @@
       </li>
       
 
+    
       <hr class="sidebar-divider">
-
-
+     
       <li class="nav-heading">Pages</li>
 
       <li class="nav-item">
@@ -189,20 +189,29 @@
         </a>
       </li><!-- End Profile Page Nav -->
 
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="pages-register.html">
-          <i class="bi bi-card-list"></i>
-          <span>Register</span>
-        </a>
-      </li><!-- End Register Page Nav -->
 
       <li class="nav-item">
-        <a class="nav-link collapsed" href="pages-login.html">
+        <a class="nav-link collapsed" href="head-register.php">
+          <i class="bi bi-card-list"></i>
+          <span>Head Register</span>
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="nurse-register.php">
+          <i class="bi bi-card-list"></i>
+          <span>Nurse Register</span>
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="pages-login.php">
           <i class="bi bi-box-arrow-in-right"></i>
           <span>Login</span>
         </a>
-      </li><!-- End Login Page Nav -->
+      </li>
 
+   
       <li class="nav-item">
         <a class="nav-link collapsed" href="pages-blank.html">
           <i class="bi bi-file-earmark"></i>
@@ -211,6 +220,7 @@
       </li><!-- End Blank Page Nav -->
 
     </ul>
+
 
   </aside><!-- End Sidebar-->
 
