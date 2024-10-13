98a29ff3ccd1790b4531229cc27bd99fb083d87c
Pat <patsandesu@gmail.com>
2024-10-14 01:42:19 +0800

    /

--- connection.php
+++ connection.php
@@ -1,17 +1,12 @@
 <?php
-// Database connection details
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "bcp_sms3_cms";
 
-// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Now you can use $conn to prepare statements
-?>
