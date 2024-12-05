f40199201d48df20078deb9682916aa00aec30e2
Pat <patsandesu@gmail.com>
2024-12-06 05:21:55 +0800

    .

--- medical-supplies.php
+++ medical-supplies.php
@@ -31,7 +31,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
@@ -50,26 +49,31 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
   <link href="assets/vendor/simple-datatables/style.css" rel="stylesheet">
   <link href="assets/css/style.css" rel="stylesheet">
+  <script type="javascript" src="assets/js/darkmode.js" defer></script>
 </head>
-
 <body>
+
+  
+  <!-- ======= Header ======= -->
   <header id="header" class="header fixed-top d-flex align-items-center">
+
     <div class="d-flex align-items-center justify-content-between">
       <i class="bi bi-list toggle-sidebar-btn"></i>
-    </div><!-- End Logo -->
+    </div>
 
     <nav class="header-nav ms-auto">
       <ul class="d-flex align-items-center">
+
         <li class="nav-item dropdown pe-3">
-          <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-            <img src="assets/img/default profile.jpg" alt="Profile" class="rounded-circle">
-            <span class="d-none d-md-block dropdown-toggle ps-2"><?php echo htmlspecialchars($fullname); ?></span>
-          </a><!-- End Profile Image Icon -->
+
+        <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
+          <img src="assets/img/default profile.jpg" alt="Profile" class="rounded-circle">
+          <span class="d-none d-md-block dropdown-toggle ps-2"><?php echo htmlspecialchars($fullname); ?></span>
+      </a>
 
           <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
             <li class="dropdown-header">
-              <h6>Kevin Anderson</h6>
-              <span>Web Designer</span>
+              <h6>Administrator</h6>
             </li>
             <li>
               <hr class="dropdown-divider">
@@ -111,7 +115,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 <span>Sign Out</span>
               </a>
             </li>
-
           </ul>
         </li>
       </ul>
