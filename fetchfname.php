5f59bba26f5771adcb5e1540d5d601cf07149948
Pat <patsandesu@gmail.com>
2025-02-18 00:49:12 +0800

    cleaning the code

--- fetchfname.php
+++ fetchfname.php
@@ -6,7 +6,7 @@ if (!isset($_SESSION['accountId'])) {
 
 include 'connection.php';
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
