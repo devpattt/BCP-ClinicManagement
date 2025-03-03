6c2af91ebdca43b60ef6ae5b198313ca659e1ec2
Pat <patsandesu@gmail.com>
2025-03-04 01:08:21 +0800

    /

--- Forecasting_AI/loaddata.py
+++ Forecasting_AI/loaddata.py
@@ -2,10 +2,14 @@ import pandas as pd
 
 df = pd.read_csv("C:/xampp/htdocs/BCP-ClinicManagement/Forecasting_AI/dands.csv")
 
+symptom_columns = df.columns.tolist()
+print("All symptoms and target column:\n", symptom_columns)
+
+symptom_columns_only = [col for col in df.columns if col != 'diseases']
+print("All symptoms:\n", symptom_columns_only)
+
 print("Missing values:\n", df.isnull().sum())
 
-symptom_columns = [col for col in df.columns if col not in ['diseases']]  
-
-df = pd.get_dummies(df, columns=symptom_columns)
+df = pd.get_dummies(df, columns=symptom_columns_only)
 
 print(df.head())
